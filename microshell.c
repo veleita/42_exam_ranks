883b459bbc5b3a0a80da5da4f142db5c5545ac00
mzomeno- <misaclumsy@gmail.com>
2021-06-14 17:53:02 +0200

    Everything coded, anything tested

--- microshell.c
+++ microshell.c
@@ -1,6 +1,7 @@
 #include <string.h> // strcmp
 #include <stdlib.h> // malloc, free
-#include <unistd.h> // chdir
+#include <unistd.h> // chdir, fork, execve, dup, dup2, pipe
+#include <sys/wait.h> // waitpid
 
 #define FATAL_ERR "error: fatal \n"
 #define CD_ARGS_ERR "error: cd: bad arguments\n"
@@ -15,7 +16,6 @@ static void put_error(char *msg)
 {
 	while (*msg != '\0')
 		write(2, msg++, 1);
-	return (1);
 }
 
 static void	syscall_fail(char *msg, char **ptr)
@@ -70,6 +70,18 @@ static char **get_next_cmd(char **argv, int *it)
 	return (ret);
 }
 
+static char **find_next_pipe(char **cmd)
+{
+	int it = -1;
+
+	while (cmd[++it])
+	{
+		if (!strcmp(cmd[it], "|"))
+			return (&cmd[it + 1]);
+	}
+	return (NULL);
+}
+
 /*
 ** EXECUTION FUNCTIONS
 */
@@ -92,14 +104,69 @@ static void exec_cd(char **cmd)
 
 static void execute(char **cmd, char **envp)
 {
+	if (execve(cmd[0], cmd, envp) == -1)
+	{
+		put_error(EXECVE_ERR);
+		put_error(cmd[0]);
+		syscall_fail("\n", cmd);
+	}
 }
 
-static void exec_generic(char **argv, char **envp)
+static void exec_generic(char **cmd, char **envp)
 {
-	if (!go_to_next_pipe(cmd)) // Case no pipes
-		execute(cmd, envp);
+	pid_t pid;
+
+	if (!find_next_pipe(cmd)) // Case no pipes
+	{
+		if ((pid = fork()) == -1) 
+			syscall_fail(FATAL_ERR, cmd);
+
+		if (pid == 0) // Child process
+			execute(cmd, envp);
+	}
 	else // Case pipes
 	{
+		int fd_in;
+		int fd_pipe[2];
+		int n_commands = 0;
+		char **simple_cmd = cmd;
+
+		while (simple_cmd)
+		{
+			if (pipe(fd_pipe) == -1 || (pid = fork()) == -1) 
+				syscall_fail(FATAL_ERR, cmd);
+
+			if ((fd_in = dup(0)) == -1)
+				syscall_fail(FATAL_ERR, cmd);
+
+			if (pid == 0) // child process
+			{
+				if (find_next_pipe(simple_cmd) && dup2(0, fd_pipe)) // If there is a pipe after this command, redirect output to the pipe
+					syscall_fail(FATAL_ERR, cmd);
+				
+				close(fd_in);
+				close(fd_pipe[0]);
+				close(fd_pipe[1]);
+
+				simple_cmd[size_till_end(simple_cmd, "|")] = NULL; // Trims command
+				execute(simple_cmd, envp);
+
+				free(cmd);
+			}
+			else
+			{
+				if (dup2(fd_pipe[0], fd_in) == -1) // Input for the next command will come from the pipe
+					syscall_fail(FATAL_ERR, cmd);
+				
+				close(fd_pipe[0]);
+				close(fd_pipe[1]);
+				n_commands++;
+				simple_cmd = find_next_pipe(simple_cmd);
+			}
+		}
+		close(fd_in);
+		while (n_commands-- >= 0)
+			waitpid(0, NULL, 0);
 	}
 }
 
@@ -110,7 +177,7 @@ int main(int argc, char **argv, char **envp)
 
 	while (++it < argc)
 	{
-		cmd = get_next_cmd(argv, &it); // i is modified so that argv[i] is set to the next ";"
+		cmd = get_next_cmd(argv, &it); // it is modified so that argv[it] is set to the next ";"
 
 		if (cmd)
 		{
